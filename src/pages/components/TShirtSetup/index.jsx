f6ce53c3711cb0e26ce4856d870aefde348a4f67
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-16 17:27:43 +0200

    morecolors+oprava onChange

--- src/pages/components/TShirtSetup/index.jsx
+++ src/pages/components/TShirtSetup/index.jsx
@@ -1,14 +1,13 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({ onTypeChange, onColorChange, onPrintChange }) => {
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
       <select
         className="tshirt-setup__input"
-        onChange={(e) => (onTypeChange = e.target.value)}
+        onChange={(e) => onTypeChange(e.target.value)}
       >
-        console.log('Type changed to:', e.target.value);
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -20,12 +19,17 @@ export const TShirtSetup = () => {
       <label className="tshirt-setup__label">Barva: </label>
       <select
         className="tshirt-setup__input"
-        onChange={(e) => (onColorChange = e.target.value)}
+        onChange={(e) => onColorChange(e.target.value)}
       >
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
         <option value="green">Zelená</option>
+        <option value="violet">Fialová</option>
+        <option value="pink">Růžová</option>
+        <option value="gold">Zlatá</option>
+        <option value="silver">Stříbrná</option>
+        <option value="orangered">Oranžovočervená</option>
       </select>
 
       <label className="tshirt-setup__label">Potisk:</label>
@@ -33,7 +37,7 @@ export const TShirtSetup = () => {
         className="tshirt-setup__input"
         type="text"
         defaultValue="Ahoj světe!"
-        onChange={(e) => (onPrintChange = e.target.value)}
+        onChange={(e) => onPrintChange(e.target.value)}
       />
     </form>
   );
